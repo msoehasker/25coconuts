d7e9acf84fe55dce88adc455209ce3aa90cbdab6
Dr. Hasker <19961794+msoehasker@users.noreply.github.com>
2025-02-26 09:30:31 -0600

    clarified role of hittable island objects

--- src/coconuts/HittableIslandObject.java
+++ src/coconuts/HittableIslandObject.java
@@ -2,8 +2,7 @@ package coconuts;
 
 import javafx.scene.image.Image;
 
-// Represents island objects which can be hits; this class will
-// implement the subject side of the pattern
+// Represents island objects which can be hit
 public abstract class HittableIslandObject extends IslandObject {
     public HittableIslandObject(Sky sky, int x, int y, int width, Image image) {
         super(sky, x, y, width, image);
